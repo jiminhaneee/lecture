091d28d9124573768412d8e8b25223be663e103e
지민 <149668162+jiminhaneee@users.noreply.github.com>
2025-01-22 16:51:05 +0900

    Init Busan

---
+++ busan/src/main/java/com/kh/busan/api/model/service/BusanServiceImpl.java
@@ -0,0 +1,79 @@
+package com.kh.busan.api.model.service;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.kh.busan.api.model.mapper.CommentMapper;
+import com.kh.busan.api.model.vo.CommentDTO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BusanServiceImpl implements BusanService {
+	
+	private final CommentMapper mapper; 
+
+	@Override
+	public String getBusan(int page) {
+		String requestUrl = "http://apis.data.go.kr/6260000/FoodService/getFoodKr";
+		   requestUrl += "?serviceKey=YzGqdkcA4isWdSx3k0R7ELhNp2FSOuM3jS1Bpas5CX6O8tIWS0gtoG9dNFZ4l66x8r04C%2B2ybs0riAxJT7vxxA%3D%3D";
+		   requestUrl += "&numOfRows=6";
+		   requestUrl += "&pageNo="+page;
+		   requestUrl += "&resultType=json";
+		URI uri = null;
+		try {
+			uri = new URI(requestUrl);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		RestTemplate restTemplate = new RestTemplate();
+		String response = restTemplate.getForObject(uri, String.class);
+		
+		return response;
+	}
+
+	@Override
+	public String getBusanDetail(int pk) {
+		String requestUrl = "http://apis.data.go.kr/6260000/FoodService/getFoodKr";
+		   requestUrl += "?serviceKey=YzGqdkcA4isWdSx3k0R7ELhNp2FSOuM3jS1Bpas5CX6O8tIWS0gtoG9dNFZ4l66x8r04C%2B2ybs0riAxJT7vxxA%3D%3D";
+		   requestUrl += "&numOfRows=10";
+		   requestUrl += "&pageNo"+ 1;
+		   requestUrl += "&resultType=json";
+		   requestUrl += "&UC_SEQ="+pk;
+		   
+		URI uri = null;
+		try {
+			uri = new URI(requestUrl);
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		RestTemplate restTemplate = new RestTemplate();
+		String response = restTemplate.getForObject(uri, String.class);
+		
+		return response;
+	}
+
+	@Override
+	public void save(CommentDTO comment) {
+		if(comment.getWriter().equals("") || comment.getContent().equals("")) {
+			System.out.println("예외가 발생했어야함");
+		}
+		mapper.save(comment);
+	}
+
+	@Override
+	public List<CommentDTO> getComments(Long foodNo) {
+		
+		return mapper.getComments(foodNo);
+	}
+	
+	
+	
+
+}
---
+++ busan/src/main/java/com/kh/busan/api/model/vo/CommentDTO.java
@@ -0,0 +1,27 @@
+package com.kh.busan.api.model.vo;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class CommentDTO implements Serializable{
+	
+	private Long foodNo;
+	@NotBlank(message = "ID는 필수 입력값입니다.")
+	private String writer;
+	private String content;
+	
+
+}
